f8490de7e8a7449ee227335f673a14436d2ae0eb
Sourish Sarmah <sourishsarmah@gmail.com>
2019-04-18 13:01:50 +0530

    Fuction for generator added

--- preprocess_data.py
+++ preprocess_data.py
@@ -1,19 +1,22 @@
 from tensorflow.keras.preprocessing.image import ImageDataGenerator
 
-train_datagen = ImageDataGenerator(rotation_range=15,
-                                   width_shift_range=0.1,
-                                   height_shift_range=0.1,
-                                   shear_range=0.01,
-                                   zoom_range=[0.9, 1.25],
-                                   horizontal_flip=True,
-                                   vertical_flip=False,
-                                   fill_mode='reflect',
-                                   data_format='channels_last',
-                                   brightness_range=[0.5, 1.5],
-                                   rescale=1./255)
+datagen = ImageDataGenerator(rotation_range=15,
+                             width_shift_range=0.1,
+                             height_shift_range=0.1,
+                             shear_range=0.01,
+                             zoom_range=[0.9, 1.25],
+                             horizontal_flip=True,
+                             vertical_flip=False,
+                             fill_mode='reflect',
+                             data_format='channels_last',
+                             brightness_range=[0.5, 1.5],
+                             rescale=1./255)
 
-train_generator = train_datagen.flow_from_directory(
-    'food-dataset/',
-    target_size=(224, 224),
-    batch_size=64,
-    class_mode='categorical')
+
+def generator(dir):
+    generator = datagen.flow_from_directory(
+        dir,
+        target_size=(224, 224),
+        batch_size=64,
+        class_mode='categorical')
+    return generator
