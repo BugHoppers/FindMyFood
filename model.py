cbd1095884c28d2828d50623ec49570e4b3db5d2
AdirthaBorgohain <adirthaborgohain@gmail.com>
2019-04-20 21:33:37 +0530

    Displayed image along with prediction and removed redundant modules

--- model.py
+++ model.py
@@ -1,14 +1,11 @@
-from keras.models import Sequential, Model
-from keras.layers import Dense, Dropout, Flatten, GlobalAveragePooling2D
+
+from keras.models import Model
+from keras.layers import Dense, GlobalAveragePooling2D
 from keras.applications import MobileNet
-
-from preprocess_data import generator
-
-import io
 from keras.preprocessing.image import load_img
-import numpy as np
+from preprocess_data import generator
 from tensorflow.keras.preprocessing import image
-
+import numpy as np
 
 def buildmodel(N):
 
@@ -35,24 +32,25 @@ def buildmodel(N):
     return model
 
 
-def train(dir):
-    train_generator = generator(dir)
-    no_of_classes = len(train_generator.class_indices)
+# def train(dir):
+#     train_generator = generator(dir)
+#     no_of_classes = len(train_generator.class_indices)
 
-    model = buildmodel(no_of_classes)
+#     model = buildmodel(no_of_classes)
 
-    model.fit_generator(train_generator, steps_per_epoch=len(
-        train_generator)/64, epochs=10)
+#     model.fit_generator(train_generator, steps_per_epoch=len(
+#         train_generator)/64, epochs=10)
     
-    model.save('model.h5')
+#     model.save('model.h5')
 
-    return 
+#     return 
     
 def predict(model, imagePath):
     img = image.load_img(imagePath, target_size=(224, 224))
     img_pred = image.img_to_array(img)                    # (height, width, channels)
+    img_array = img_pred
     img_pred = np.expand_dims(img_pred, axis=0)  
     img_pred = img_pred/255.
     preds = model.predict(img_pred)
     y_classes = preds.argmax(axis=-1)
-    return y_classes
+    return y_classes, img_array
