eaf05b5ac381a77024a282d08d6da846da9fce74
Sourish Sarmah <sourishsarmah@gmail.com>
2019-04-20 11:20:29 +0530

    Added train function

--- model.py
+++ model.py
@@ -2,6 +2,8 @@ from keras.models import Sequential, Model
 from keras.layers import Dense, Dropout, Flatten, GlobalAveragePooling2D
 from keras.applications import MobileNet
 
+from preprocess_data import generator
+
 
 def buildmodel(N):
 
@@ -24,5 +26,19 @@ def buildmodel(N):
 
     model.compile(optimizer='Adam',
                   loss='categorical_crossentropy', metrics=['accuracy'])
+
+    return model
+
+
+def train(dir):
+    train_generator = generator(dir)
+    no_of_classes = len(train_generator.class_indices)
+
+    model = buildmodel(no_of_classes)
+
+    model.fit_generator(train_generator, steps_per_epoch=len(
+        train_generator)/64, epochs=10)
+    
+    model.save('model.h5')
 
     return model
